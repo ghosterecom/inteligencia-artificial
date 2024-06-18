fd407da9944a5cbd97004bf6bfb47038ddca1702
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-06-18 11:34:55 -0300

    1806

---
+++ script.js
@@ -0,0 +1,26 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaperguntas = Document.querySelector('.caixaperguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaresultados = Document.querySelector('.caixa-resultados');
+const textoresulados = document.querySelector('.texto-resultado');
+const lista = [item1, item2]
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alterntivas: ["alternativa 1, alternativa 2"],
+    },
+    {
+        enunciado: "pergunta 2",
+        alterntivas: ["alternativa 1, alternativa 2"],
+    }
+    {
+        enunciado: "pergunta 3",
+        alterntivas: ["alternativa 1, alternativa 2"],
+    }
+];
+const lapis = {
+    tamanho: 20,
+    tipo: 'HB',
+    cor 'Grafite',
+    TemBorrachaAtras: false
+}
