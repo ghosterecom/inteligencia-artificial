6123e76e36d35b899864de2c761f869f3680246e
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-06-19 15:12:52 -0300

    1906

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-const caixaprincipal = document.querySelector('.caixa-principal');
-const caixaperguntas = Document.querySelector('.caixaperguntas');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixaperguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
-const caixaresultados = Document.querySelector('.caixa-resultados');
-const textoresulados = document.querySelector('.texto-resultado');
+const caixaResultados = document.querySelector('.caixa-resultados');
+const textoResulados = document.querySelector('.texto-resultado');
 const lista = [item1, item2]
 const perguntas = [
     {
-        enunciado: "pergunta 1",
-        alterntivas: ["alternativa 1, alternativa 2"],
+        enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
+        alterntivas: ["Isso é assustador!, Isso é maravilhoso!"],
     },
     {
         enunciado: "pergunta 2",
         alterntivas: ["alternativa 1, alternativa 2"],
-    }
+    },
     {
         enunciado: "pergunta 3",
         alterntivas: ["alternativa 1, alternativa 2"],
-    }
+    },
 ];
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.Alternativas){
+        const botaoAlternativas =document.createElement("button");
+        botaoAlternativas.textContent = alterntiva;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
 const lapis = {
     tamanho: 20,
     tipo: 'HB',
-    cor 'Grafite',
+    cor: 'Grafite',
     TemBorrachaAtras: false
 }
