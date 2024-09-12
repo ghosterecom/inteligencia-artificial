ae94321c388b11bd1774c588cd11de38d97f286d
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-09-12 07:43:40 -0300

    18889

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = ["Ana", "Fernanda", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+
 export function aleatorio (lista){
-    //trecho do codico suprimido
-}
-const nomes = ["sergio", "igor", "gabriel"];
+    const posicao = Math.floor(Math.random()*lista.length);   
+    return lista [posicao];
+ }
+
+ export const nome = aleatorio(nomes)
