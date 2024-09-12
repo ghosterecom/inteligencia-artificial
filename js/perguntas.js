4d3f79e4fd315ef938e3f65d7ce4425d56c1b1de
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-09-12 08:52:09 -0300

    456789

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,90 +1,90 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que entrou em sua lavoura de soja, se depara com uma multidão de lagartas.",
         alternativas: [
             {
                 texto: "Isso é assustador!",
                 afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer. "
+                    "No início ficou com medo, de prejudicarem sua lavoura "
                 ],
                 proxima: 1,
                 },
             {
                 texto: "Isso é maravilhoso!",
                 afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia."
+                    "Pois ele sabia qual agrotoxico ele iria aplicar para o controle das lagartas."
                 ],
                 proxima: 2,
                 }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Com a descoberta destes novos agrotoxicos para o uso na lavoura, ajudando o controle de pragas e plantas daninhas.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Utiliza uma ferramenta chamada de autopropelido para a aplicação de fungicidas e demais agrotoxicos em sua lavoura.",
                 afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis."
+                    "Conseguiu realizar todas as suas aplicações em todas as suas areas de risco de perdas por pragas."
                 ],
                 proxima: 3,
                 },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: "Realizou o trabalho com base nas suas conversas que teve mais cedo com seus amigos agronomos.",
                 afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                    "Sentiu mais facilidade em realizar o trabalho com os novos produtos recomendados pelos seus amigos agronomos."
                 ],
                 proxima: 4,
                 }
         ]
     },
     {
-        enunciado: "Depois que você escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro. O que você faz?",
+        enunciado: "Depois que ele realizou todas as suas aplicações ao chegar em casa teve uma discução com sua esposa.",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Ele defende seus amigos agronomos pois os produtos recomendados por eles, ajudou bastante suas lavouras.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                    "Vem impulsionando o uso de novos agrotoxicos."
                 ],
                 proxima: 5,
                 },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Me preocupo com o alto custo de produtos que foram aplicados.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                    "Sua preocupação e o investimento alto e a produção baixa."
                 ],
                 proxima: 6,
                 }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Ao final da discussão, a sua esposa entendeu que era para o bem da sua lavoura.",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                texto: "ele á esplicou que aplicando os novos produtos mesmo com o custo elevado teria uma producão alta.",
                 afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                    "Acelerou o processo de sua lavoura pois os fertilizantes eram precosses."
                 ],
                 proxima: 7,
                 },
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                texto: "ele vai ajudar os seus amigos agronomos na divulgação dos produtos.",
                 afirmacao: [
-                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                    "Notou também que muitas pessoas não sabem ainda utilizar os produtos de lançamento, e ele conseguiu fornecer os seus conhecimentos."
                 ],
                 proxima: 7,
                 }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "ele teve o trabalho de participar na divulgação do produto indicado pelos seus amigos agronomos. ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "ele teve uma boa produção graças aos produtos indicados pelos agronomos.",
+                afirmacao: "felismente ele teve uma otima produção."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Os produtos tem uma nova tecnologia que traz muitos beneficios para a lavoura.",
+                afirmacao: "Percebeu que todas as pessoas que utilizaram os novos produtos tiveram um otimo resutado na produção que foi aplicado o produto. "
             },
         ]
     },
