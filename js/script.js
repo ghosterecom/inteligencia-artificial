46bd98ed2f20b870dae80bb791e9c6f12772f8d0
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-10-03 07:41:53 -0300

    8765

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ function mostraResultado() {
 function jogaNovamente() {
     atual = 0;
     historiaFinal = "";
-    caixaResultado.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
     mostraPergunta();
 }
 
